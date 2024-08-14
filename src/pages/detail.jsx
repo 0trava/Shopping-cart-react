08d4121d79e84f111edf41996d853ed064ab199b
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2024-08-14 17:45:40 +0300

    add to page detail buttons

--- src/pages/detail.jsx
+++ src/pages/detail.jsx
@@ -31,6 +31,15 @@ const Detail = () => {
           <p className='font-bold text-3xl'>
             ${detail.price}
           </p>
+          <div className='flex gap-5'>
+            <div className='flex gap-2 justify-center items-center'>
+              <button className='bg-gray-100 h-full w-10 font-bold text-xl rounded-xl flex justify-center items-center'>-</button>
+              <span className='bg-gray-200 h-full w-10 font-bold text-xl rounded-xl flex justify-center items-center'>1</span>
+              <button className='bg-gray-100 h-full w-10 font-bold text-xl rounded-xl flex justify-center items-center'>+</button>
+            </div>
+            <button className='bg-gray-900 text-white px-7 py-3 rounded-xl shadow-2xl'>Add To Cart</button>
+          </div>
+          <p>{detail.description}</p>
         </div>
       </div>
     </div>
